f07ff6c18d44e25a9a6641ec9a7d02ede2050433
KIM SANGYEUB <kim.sangyeub@rooter.co.jp>
2019-05-21 17:13:21 +0900

    init

---
+++ ls.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main(int argc, char* argv[]){
+  DIR *dp;
+  struct dirent *dir;
+  char *path = ".";
+
+  if((dp = opendir(path)) == NULL){
+    perror("Can't open directory");
+    exit(1);
+  }
+
+  while((dir = readdir(dp)) != NULL){
+    if(dir -> d_ino == 0){   /* skip remove file */
+      continue;
+    }
+    (void) printf("%llu\t%d\t%hhu\t%s\n", dir -> d_ino, dir -> d_reclen, dir -> d_type, dir -> d_name);
+  }
+
+  (void) closedir(dp);
+
+  exit(0);
+}
