a25ecadadaf4dca18c2ba17a7fe7734b568a6263
KIM SANGYEUB <kim.sangyeub@rooter.co.jp>
2019-05-26 22:51:19 +0900

    add cmpstr and qsort

--- ls_new.c
+++ ls_new.c
@@ -19,6 +19,8 @@ struct ls_st {
 struct ls_st LS_LIST[2000];
 int LS_INDEX = 0;
 
+int cmpstr(const void* a, const void* b);
+
 int main(int argc, char* argv[]){
   DIR* dp;
   char* path = ".";
@@ -57,6 +59,7 @@ int main(int argc, char* argv[]){
     //       ctime(&ls.mtime),
     //       ls.name);
   }
+  qsort(LS_LIST, LS_INDEX, sizeof(ls_st), cmpstr)
   for(int i = 0; i < LS_INDEX; i++){
     printf("%lld\t%ld\t%ld\t%ld\t%lld\t%s\t%s\n",
           (long long) LS_LIST[i].mode,
@@ -67,4 +70,11 @@ int main(int argc, char* argv[]){
           ctime(&LS_LIST[i].mtime),
           LS_LIST[i].name);
   }
+  return 0;
+}
+
+int cmpstr(struct ls_st a, struct ls_st b){
+  const char* aa = *(const char**)a.name;
+  const char* bb = *(const char**)b.name;
+  return strcmp(aa, bb);
 }
